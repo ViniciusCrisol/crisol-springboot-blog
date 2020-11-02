ca0f546007140956a329df1a309b0ba2a8ac6620
Vinícius Poloni Crisol <vinicius.crisol@hotmail.com>
2020-11-02 20:01:08 -0300

    first commit

---
+++ src/main/java/com/spring/crisolblog/CrisolblogApplication.java
@@ -0,0 +1,13 @@
+package com.spring.crisolblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrisolblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrisolblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/crisolblog/CrisolblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.crisolblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrisolblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
